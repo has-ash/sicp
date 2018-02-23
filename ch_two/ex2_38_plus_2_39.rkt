e1b05429e6c458a5d870300ab28b1cfa81d7a929
hasan112 <hasanash112@gmail.com>
2018-02-24 00:37:59 +0100

    added ex2_39

--- ch_two/ex2_38_plus_2_39.rkt
+++ ch_two/ex2_38_plus_2_39.rkt
@@ -28,4 +28,24 @@
 ; One property that immediately jumps out is associativity
 ; since we're doing operations in a different order and
 ; that means the operations should be associative for the result to be
-; the same if we start from either end. 
+; the same if we start from either end.
+
+
+; ex 2_39
+
+(define (reverse-right sequence)
+  (accumulate (lambda (x y) (if (null? y)
+                                (list x)
+                                (append y (list x) ) ) )
+              nil sequence) )
+
+(define (reverse-left sequence)
+  (fold-left (lambda (x y) (if (null? x)
+                               (list y)
+                               (append (list y) x ) ) )
+             nil sequence ) ) 
+
+(define sequence (list 1 2 3 4 5) )
+
+(reverse-right sequence)
+(reverse-left sequence) 
